3758350489ee95f387118d6ec0b2d159bf0e26d8
Nathan <nslee333@gmail.com>
2023-07-29 17:47:08 -0700

    :-)

--- test.js
+++ test.js
@@ -1,28 +1,38 @@
 const linkify = require("linkify-it")()
 
 // let content = "@everyone github.com"
-let content = "@everyone github.com github.com"
+let content = "@everyone github.com"
 // ? Goal is this -> "@everyone <github.com>"
 
 
-let arr = content.split("")
+// let arr = content.split("")
 
 
-let resultLength = linkify.match(content).length
+// let resultLength = linkify.match(content).length
 
-for (let i = 0; i < resultLength; i++ ) {
-  let result = linkify.match(content)
-  // result = linkify.match(content)
-  // console.log(result[j])
+// for (let i = 0; i < resultLength; i++ ) {
+  //   let result = linkify.match(content)
   
-  // console.log(result[i].index)
-  arr.splice(result[i].index, 0, "<")
-  arr.splice(result[i].lastIndex + 1, 0, ">")
+  //   arr.splice(result[i].index, 0, "<")
+  //   arr.splice(result[i].lastIndex + 1, 0, ">")
+  
+  //   content = arr.join("")
+  // }
+  
+  
+  // console.log(content)
+  
+const matches = linkify.match(content);
 
-  content = arr.join("")
+let offset = 0;
+for (const match of matches) {
+  const linkStart = match.index + offset;
+  const linkEnd = match.lastIndex + offset;
+
+  const linkified = `<${match.text}>`;
+  content = content.slice(0, linkStart) + linkified + content.slice(linkEnd + 1);
+
+  offset += linkified.length - (match.lastIndex - match.index + 1);
 }
 
-
-// console.log(arr.length)
-// console.log(result)
-console.log(content)
+console.log(content);
